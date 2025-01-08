98f260ab6bf688803053385f4501ec2a94092c2e
madrilene <hola@lenesaile.com>
2025-01-08 17:04:30 +0100

    remove 440 px for images

--- src/_config/shortcodes/image.js
+++ src/_config/shortcodes/image.js
@@ -22,7 +22,7 @@ export const imageShortcode = async (
   loading = 'lazy',
   className,
   sizes = '100vw',
-  widths = [440, 650, 960, 1200],
+  widths = [650, 960, 1200],
   formats = ['webp', 'jpeg']
 ) => {
   const metadata = await Image(src, {
